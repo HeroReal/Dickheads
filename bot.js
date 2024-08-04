b0a7108d42505a7fec8107a11974b98a4b24b896
HeroReal <ardilkasacibackup@gmail.com>
2024-08-04 03:16:29 +0300

    bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+  console.log('Bot ist bereit!');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong.');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
